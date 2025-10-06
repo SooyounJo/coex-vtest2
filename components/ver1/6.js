ed6b931f77a2d12bd897249c54d45edcfaba7456
SooyounJo <dus10167@gmail.com>
2025-10-07 04:13:26 +0900

    new type

--- components/ver1/6.js
+++ components/ver1/6.js
@@ -1,17 +1,5 @@
-import { useFrame } from '@react-three/fiber'
-import AgenticBubble from './1'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useMemo, useRef } from 'react'
+import * as THREE from 'three'
 
-export default function Style6Zoom() {
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime()
-    const period = 11.0
-    const baseZ = 6.0
-    const amplitudeZ = 0.6
-    const z = baseZ + amplitudeZ * Math.sin((2 * Math.PI * t) / period)
-    state.camera.position.z = z
-    state.camera.updateProjectionMatrix()
-  })
-
-  return <AgenticBubble styleType={6} />
-}
-
+export default function ShaderBubble6Blank() { return null }
